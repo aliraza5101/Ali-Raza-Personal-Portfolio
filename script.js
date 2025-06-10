f5ba71374c878f3099f4e29c1ebc60681b1e3b2c
Ali Raza Qureshi <alirazaqureshi310251@gmail.com>
2025-06-10 13:08:31 +0500

    script.js

--- script.js
+++ script.js
@@ -260,4 +260,13 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(contactSection);
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector("nav");
+
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+});
+
 
